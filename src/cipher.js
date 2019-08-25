62d70daa76c06bf0f7dbf76bccd2fcd653c4f2d2
andreaaguilar3a <wanderlust3a@gmail.com>
2019-08-25 18:22:00 -0400

    subir css

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,32 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
+    encode: (textEncode, numberOffset) => {
+      let result = "";
+      for (let i = 0; i < textEncode.length; i++) {
+  
+        //variable de la Fórmula para posicionar la letra dentro del código ASCII
+        let letter = (textEncode.charCodeAt(i) - 65 + parseInt(numberOffset)) % 26 + 65;
+  
+        //Concatenando letras codificadas
+        result += String.fromCharCode(letter);
+      }
+  
+      return result;
+  
+    },
+  
+    decode: (textDecode, numberOffset) => {
+      let result = "";
+      for (let i = 0; i < textDecode.length; i++) {
+  
+        //variable de la Fórmula para posicionar la letra dentro del alfabeto  
+        let letter = (textDecode.charCodeAt(i) + 65 - parseInt(numberOffset)) % 26 + 65;
+  
+        //Concatenando las letras decodificadas
+        result += String.fromCharCode(letter);
+  
+      }
+  
+      return result;
+  
+    }
   }
-};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,88 @@
-/*Acá va tu código*/
+window.onload = () => {
+
+    /*activado y desactivado de los enlaces de cada pestaña*/
+
+    const linkActive = document.getElementById("link-encode");
+    const linkDesactive = document.getElementById("link-decode");
+
+    linkActive.addEventListener("click", function() {
+        document.getElementById("link-encode").classList.add("link_active");
+        document.getElementById("link-decode").classList.remove("link_active");
+        document.getElementById("content-msg-encode").classList.remove("display_none");
+        document.getElementById("content-msg-encode").classList.add("display_block");
+        document.getElementById("content-msg-decode").classList.remove("display_block");
+        document.getElementById("content-msg-decode").classList.add("display_none");
+    });
+
+    linkDesactive.addEventListener("click", function() {
+        document.getElementById("link-encode").classList.remove("link_active");
+        document.getElementById("link-decode").classList.add("link_active");
+        document.getElementById("content-msg-decode").classList.remove("display_none");
+        document.getElementById("content-msg-decode").classList.add("display_block");
+        document.getElementById("content-msg-encode").classList.remove("display_block");
+        document.getElementById("content-msg-encode").classList.add("display_none");
+       
+    });
+   //boton limpiar en cifrado
+     
+
+   document.getElementById("clean-encode").addEventListener ("click", () => {
+
+    document.getElementById ("box-msg-encode").value="";
+    document.getElementById ("n_offset_e").value= ""; 
+    document.getElementById ("msg-encode-check").value="";
+
+
+    });
+    // boton limpiar decifrado
+    document.getElementById("clean-decode").addEventListener ("click",() => {
+
+        document.getElementById("box-msg-decode").value= "";
+        document.getElementById("n_offset_d").value="";
+    });
+
+    /*rescatando el valor del texto a codificar */
+
+    // variable boton
+    const btnTextEncode = document.getElementById("btn_encode");
+   
+    //evento click cifrar
+    btnTextEncode.addEventListener("click", () => {
+       
+        let textEncode = document.getElementById("box-msg-encode").value.toUpperCase ();
+        let numberOffset = document.getElementById("n_offset_e").value;
+        //let encodeText = window.cipher.encode(texEncode, numberOffset) //se mandan los valores de la funcion 
+        
+        document.getElementById("msg-encode-check").innerHTML = window.cipher.encode(textEncode, numberOffset);
+        //document.getElementById("msg-encode-check").value = encodedText;
+    });
+    
+
+    /*rescatando el valor del texto a decodificar*/
+
+    //variable botón
+    const btnTextDecode = document.getElementById("btn_decode");
+    
+    //evento click decifrar
+    btnTextDecode.addEventListener("click", () => {
+       
+        let textDecode = document.getElementById("box-msg-decode").value.toUpperCase();
+        let numberOffset = document.getElementById("n_offset_d").value;
+        //let decodeText= window.cipher.decode(textDecode, numberOffset);
+        document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, numberOffset);
+        //document.getElementById("msg-decode-check").value = decodeText;
+    });
+
+     
+
+
+}
+
+    
+
+
+
+
+
+
+
